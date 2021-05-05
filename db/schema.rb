14dab9234a4f2fa10d687033df464a7c6ebbd517
KennyUTN <asisignacio@gmail.com>
2021-05-05 11:14:31 -0300

    login fix?
    login fix test

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20_210_428_210_458) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
+    t.string 'encrypted_password', default: 'password', null: false
     t.string 'reset_password_token'
     t.datetime 'reset_password_sent_at'
     t.datetime 'remember_created_at'
